e9a0a71c7a4836cabd146d9f2ac2cd81649505ea
Rokas Augunas <augunasr@gmail.com>
2023-08-16 07:34:16 +0200

    feat: loading-screen

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+export const LoadingText = () => {
+  return (
+    <h1 className="text-2xl font-semibold min-h-screen bg-gray-100 flex justify-center items-center ">
+      Loading...
+    </h1>
+  );
+};
